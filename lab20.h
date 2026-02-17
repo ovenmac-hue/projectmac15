d25432a96e2b271d89125ea35674e3a4acb95a30
MacThas <ovenmac@gmail.com>
2026-02-17 21:00:06 +0700

    #

--- lab20.h
+++ lab20.h
@@ -54,7 +54,22 @@ Unit::Unit(string t,string n){
 	}
 	hp = hpmax;	
 	guard_on = false;
+	dodge_on = false;
 	equipment = NULL;
+}
+
+Equipment::Equipment(int h,int a,int d){
+	hpmax = h;
+	atk = a;
+	def = d;
+}
+
+vector<int> Equipment::getStat(){
+	vector<int> stat;
+	stat.push_back(hpmax);
+	stat.push_back(atk);
+	stat.push_back(def);
+	return stat;
 }
 
 void Unit::showStatus(){
@@ -74,13 +89,23 @@ void Unit::showStatus(){
 
 void Unit::newTurn(){
 	guard_on = false; 
+	dodge_on = false;
 }
 
 int Unit::beAttacked(int oppatk){
-	int dmg;
+	int dmg = 0;
 	if(oppatk > def){
 		dmg = oppatk-def;	
 		if(guard_on) dmg = dmg/3;
+
+		if(dodge_on) {
+			int z = rand()%2;
+			if(z == 0){
+				dmg = 0;
+			}else{
+				dmg *= 2;
+			}
+		}
 	}	
 	hp -= dmg;
 	if(hp <= 0){hp = 0;}
@@ -107,6 +132,39 @@ bool Unit::isDead(){
 	if(hp <= 0) return true;
 	else return false;
 }
+
+int Unit::ultimateAttack(Unit &opp){
+    int newatk = atk *2;
+	return opp.beAttacked(newatk);
+}
+
+void Unit::dodge(){
+	dodge_on = true;
+}
+
+void Unit::equip(Equipment *e){
+
+	if(equipment != NULL){
+		vector<int> oldStat = equipment->getStat();
+		hpmax -= oldStat[0];
+		atk   -= oldStat[1];
+		def   -= oldStat[2];
+	}
+
+	equipment = e;
+
+	vector<int> newStat = equipment->getStat();
+	hpmax += newStat[0];
+	atk   += newStat[1];
+	def   += newStat[2];
+	
+	
+	if(hp > hpmax){
+		hp = hpmax;
+	}
+}
+
+
 
 void drawScene(char p_action,int p,char m_action,int m){
 	cout << "                                                       \n";
